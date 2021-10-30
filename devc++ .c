885069ebfcbcc5eb2ae0b58b3d4bda9a522adb48
adithyajavvadi <93368346+adithyajavvadi@users.noreply.github.com>
2021-10-29 23:15:46 -0700

    Add files via upload

---
+++ devc++ .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	int num1, num2, sum;
+	
+	//initialization
+	num1 =10;
+	num2 =20;
+	
+	//calculation
+	sum=num1+num2;
+	
+	//output
+	printf("%d",sum);
+}
